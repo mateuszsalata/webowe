3af731490ce9ffa7818be31b28315fc8aa809d16
mateuszsalata <90031917+mateuszsalata@users.noreply.github.com>
2023-10-02 12:57:39 +0200

    Add files via upload

--- node.js
+++ node.js
@@ -1,11 +1,25 @@
-const http = require('http')
-const hostname = '127.0.0.1'
-const port = 3000
-const server = http.createServer((req, res) => {
- res.statusCode = 200
- res.setHeader('Content-Type', 'text/plain')
- res.end('Hello World')
-})
+const http = require('http');
+const fs = require('fs').promises;
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer(async (req, res) => {
+  const url = req.url;
+
+  if (url === "/") {
+    res.statusCode = 200;
+    const html = await fs.readFile("./index.html");
+
+    res.setHeader("content-type", "text/html");
+
+    res.write(html);
+
+    res.end();
+  }
+});
+
 server.listen(port, hostname, () => {
- console.log(`Server running at http://${hostname}:${port}/`)
-})
+  console.log(`Server running at http://${hostname}:${port}`);
+});
+/home/student/2023_09_25/node.js
